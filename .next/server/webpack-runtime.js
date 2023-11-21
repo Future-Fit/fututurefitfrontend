d3cafa5b0c36fbdf4426048cfba628e352e9b12c
Zola <mac@macs-MacBook-Pro.local>
2023-11-21 17:02:31 +0300

    Push Last update from local

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("fafee16375771be9")
+/******/ 		__webpack_require__.h = () => ("41e44f477e714579")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ba48d10d50742a8"; }
+/******/ 		__webpack_require__.h = function() { return "14f0aa4011988f91"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
