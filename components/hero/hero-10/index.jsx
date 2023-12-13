7653ac56fb2cfcc89ec96e67bbf2a8dd54b322e3
Zola <mac@macs-MacBook-Pro.local>
2023-12-13 14:07:12 +0300

    Mobile and Desktop Responsive Text decoration too

--- components/hero/hero-10/index.jsx
+++ components/hero/hero-10/index.jsx
@@ -47,33 +47,59 @@ const index = () => {
                     <>
                       <h3 style={{ fontWeight: '600', /* Other mobile styles */ }}>Welcome to FFI</h3>
                       <h2 style={{ color: "#b2dfdb", fontSize: '20px' }}> Explore Opportunties, Turn Ambitions into Reality!</h2>
+
+                      <div style={{
+                        padding: '20px 20px 20px 20px',
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                        borderRadius: '10px',
+                        display: 'inline-block',
+                        alignContent: 'start'
+                      }}>
+                        <h4 style={{
+                          color: 'white',
+                          textAlign: 'left', // Align text to the left
+                          margin: 0, // Remove any default margins
+                        }}>
+                          At FFI, our passion is enabling individuals, businesses, and institutions make great connections.
+                          Our ever-expanding global reach and expertise gives us the ability to provide growing networking and
+                          matching opportunities for job seekers, students, businesses and colleges/universities. With years
+                          of experience in talent management, international business, education and government services, we are
+                          your champions in the pursuit of your aspirations and goals.
+                        </h4>
+                      </div>
                     </>
                   ) : (
                     <>
                       <h3 style={{ fontWeight: '600', marginTop: '100px' /* Other desktop styles */ }}>Welcome to Future Fit International</h3>
                       <h2 style={{ color: "#b2dfdb" }}> Explore Opportunties, Turn Ambitions into Reality!</h2>
+
+                      <div style={{
+                        padding: '20px 20px 20px 20px',
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                        borderRadius: '10px',
+                        display: 'inline-block',
+                        marginTop: '150px',
+                        width: '1000px',
+                        alignContent: 'start'
+                      }}>
+                        <h4 style={{
+                          color: 'white',
+                          textAlign: 'left', // Align text to the left
+                          margin: 0, // Remove any default margins
+                        }}>
+                          At FFI, our passion is enabling individuals, businesses, and institutions make great connections.
+                          Our ever-expanding global reach and expertise gives us the ability to provide growing networking and
+                          matching opportunities for job seekers, students, businesses and colleges/universities. With years
+                          of experience in talent management, international business, education and government services, we are
+                          your champions in the pursuit of your aspirations and goals.
+                        </h4>
+                      </div>
                     </>
 
                   )}
 
-                  <h4 style={{color:'white', paddingTop: '30px', padding: '20px 100px 20px', textAlign: 'justify', backgroundColor: 'rgba(0, 0, 0, 0.5)'}}>
-                    At FFI, our passion is enabling individuals, businesses, and institutions make great connections.
-                    Our ever-expanding global reach and expertise gives us the ability to provide growing networking and
-                    matching opportunities for job seekers, students, businesses and colleges/universities. With years
-                    of experience in talent management, international business, education and government services, we are
-                    your champions in the pursuit of your aspirations and goals.
-                  </h4>
+
                 </div>
-
-                {/* <div className="job-search-form" data-aos-delay="700" data-aos="fade-up">
-                  <SearchForm3 />
-                </div> */}
-
-                {/* <div className="top-features">
-                  <div className="row" data-aos-delay="700" data-aos="fade-up">
-                    <JobCategorie4 isMobile={isMobile} />
-                  </div>
-                </div> */}
               </div>
             </div>
           </section>
