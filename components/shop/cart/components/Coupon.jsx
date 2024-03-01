e6e3f2232d3afd587c76ad2f385d3a871ad45ca0
Zola <mac@macs-MacBook-Pro.local>
2024-03-01 09:59:06 +0300

    coupon code updated

--- components/shop/cart/components/Coupon.jsx
+++ components/shop/cart/components/Coupon.jsx
@@ -1,4 +1,25 @@
+"use client"
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 const Coupon = () => {
+
+    const router = useRouter(); // Initialize useRouter
+    const [couponCode, setCouponCode] = useState(''); // State to track the coupon code input
+
+    // Function to handle applying coupon
+    const handleApplyCoupon = () => {
+        if (couponCode.trim() === '') {
+            alert('Please enter a coupon code');
+            console.log("Please enter a coupon code")
+        } else {
+            // Perform action to apply coupon code (if needed)
+            alert(`Coupon ${couponCode} applied!`);
+        }
+    };
+    // Function to handle button click
+    const handleAddMoreItemClick = () => {
+        router.push('/shop/shop-list'); // Navigate to '/shop/shop-list'
+    };
     return (
         <div className="apply-coupon">
             <div className="form-group">
@@ -7,17 +28,22 @@ const Coupon = () => {
                     name="coupon-code"
                     className="input"
                     placeholder="Coupon Code"
+                    value={couponCode}
+                    onChange={(e) => setCouponCode(e.target.value)} // Update couponCode state on input change
+                    required
                 />
             </div>
 
             <div className="form-group">
-                <button type="button" className="theme-btn btn-style-one">
+                <button onClick={handleApplyCoupon} type="button" className="theme-btn btn-style-one">
                     Apply Coupon
                 </button>
+                {/* Display message based on coupon code input */}
+                {/* <span>{couponCode.trim() === '' ? '' : 'Your coupon is applied'}</span> */}
             </div>
 
             <div className="form-group pull-right">
-                <button onClick={`/shop/shop-list`} type="button" className="theme-btn btn-style-three">
+                <button onClick={handleAddMoreItemClick} type="button" className="theme-btn btn-style-three">
                     Add More Item
                 </button>
             </div>
